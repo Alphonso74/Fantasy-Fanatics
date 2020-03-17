2f9c5e2eb8b5865eded4eaf40c7eef99c444501f
qtran1375 <qtran1375@gmail.com>
2020-03-17 16:50:50 -0400

    Quang, fixing intents

--- app/src/main/java/psu/ajm6684/myapplication/CreateAccountActivity.java
+++ app/src/main/java/psu/ajm6684/myapplication/CreateAccountActivity.java
@@ -10,7 +10,7 @@ public class CreateAccountActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_account);
 
 
 
--- app/src/main/java/psu/ajm6684/myapplication/MyTeamActivity.java
+++ app/src/main/java/psu/ajm6684/myapplication/MyTeamActivity.java
@@ -10,7 +10,7 @@ public class MyTeamActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.my_teams);
 
 
 

e5a105a78bb8008a70af6850c9595d13ec9ec329
qtran1375 <qtran1375@gmail.com>
2020-04-03 15:40:21 -0400

    QT added players version 1.0

--- app/src/main/java/psu/ajm6684/myapplication/AddTeamActivity.java
+++ app/src/main/java/psu/ajm6684/myapplication/AddTeamActivity.java
@@ -1,4 +1,36 @@
 package psu.ajm6684.myapplication;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class AddTeamActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+public class AddTeamActivity extends  AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.add_players);
+    }
 }

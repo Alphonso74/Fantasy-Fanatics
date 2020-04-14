8549363db5aae3e100c9dd319cb8a3ec242ea1f1
fbk5061 <56009387+fbk5061@users.noreply.github.com>
2020-04-14 01:57:59 -0400

    Foram, WIP for Animation

--- app/src/main/java/psu/ajm6684/myapplication/SplashScreenActivity.java
+++ app/src/main/java/psu/ajm6684/myapplication/SplashScreenActivity.java
@@ -7,23 +7,33 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
-        
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView2);
+
+        image.setAnimation(topAnim);
 
         new Handler().postDelayed(new Runnable(){
 

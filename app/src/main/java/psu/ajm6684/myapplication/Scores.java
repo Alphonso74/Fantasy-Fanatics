660d409f531c2b04eb232c6b07ce5d08a1d2c8e2
fbk5061 <56009387+fbk5061@users.noreply.github.com>
2020-05-03 16:18:42 -0400

    Modified Scores.java with dark mode

--- app/src/main/java/psu/ajm6684/myapplication/Scores.java
+++ app/src/main/java/psu/ajm6684/myapplication/Scores.java
@@ -20,11 +20,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -35,12 +38,15 @@ import com.google.firebase.firestore.Query;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Scores extends AppCompatActivity {
 
-    private FirebaseFirestore db;
+    //private FirebaseFirestore db;
 
     TextView ScoresList;
     private RecyclerView recyclerView;
@@ -48,6 +54,14 @@ public class Scores extends AppCompatActivity {
     String team1;
     String team2;
     Button btn;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference users = db.collection("Users");
+    FirebaseAuth firebaseAuth;
+
+    DocumentReference mode;
+    FirebaseUser current;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -92,7 +106,10 @@ public class Scores extends AppCompatActivity {
 
         ScoresList = (TextView) findViewById(R.id.scoresList);
 
-        db = FirebaseFirestore.getInstance();
+       // db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        current = firebaseAuth.getCurrentUser();
         recyclerView = findViewById(R.id.recycler_View);
 
         Query query = db.collection("Scores");
@@ -193,7 +210,45 @@ public class Scores extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
+        mode = firestore.collection("Users").document(current.getUid());
 
+        mode.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        List<String> list = new ArrayList<>();
+
+                        Map<String, Object> map = document.getData();
+                        if (map != null) {
+                            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                                list.add(entry.getValue().toString());
+                            }
+                        }
+
+
+                        for (String s : list) {
+
+                            if(s.equals("light"))
+                            {
+
+                                getDelegate().setLocalNightMode((AppCompatDelegate.MODE_NIGHT_NO));
+                                break;
+                            }
+
+                            if (s.equals("dark"))
+                            {
+                                getDelegate().setLocalNightMode((AppCompatDelegate.MODE_NIGHT_YES));
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+        });
+        //
 
 
     }
